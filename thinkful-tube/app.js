140d9e238467fdff283c03215c8a474f7c95454e
Dana Ng <ngdana1@gmail.com>
2016-12-02 16:47:55 -0800

    setup thinkful-tube challenge

---
+++ thinkful-tube/app.js
@@ -0,0 +1,46 @@
+var YOUTUBE_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  var settings = {
+    url: YOUTUBE_BASE_URL,
+    data: {
+      q: searchTerm,
+      part: 'snippet',
+      key: 'AIzaSyAuB6kqL52NjpIT8qi8y1MasDE5OXcjz7g'
+    },
+      
+      
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
+}
+
+
+
+function displaySearchData(data) {
+  var resultElement = '';
+  for(var i = 0; i < (data.items).length; i++) {
+  var dataInfo = data.items[i].snippet;
+  var description = dataInfo['description']; 
+  var src = dataInfo['thumbnails']['medium']['url'];
+  var link = dataInfo["channelId"];
+   console.log(dataInfo);
+  resultElement += '<div class="items"><a href="https://www.youtube.com/channel/'+ link +'" target="_blank"><img src="'+ src +'" class="thumbnails"></a>' + '</div>';
+  
+  }
+  
+  $('.js-search-results').html(resultElement);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function(e) {
+    e.preventDefault();
+    var query = $(this).find('.js-query').val();
+    resultElement = '';
+    getDataFromApi(query, displaySearchData);
+  });
+}
+
+$(function(){watchSubmit();});
