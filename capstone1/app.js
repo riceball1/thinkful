7299a1e397684250f30b001fe3813dff9f764717
Dana Ng <ngdana1@gmail.com>
2016-12-06 22:05:27 -0800

    added function for api - still tweaking

--- capstone1/app.js
+++ capstone1/app.js
@@ -2,12 +2,40 @@
 
 function tweeterList() {
 
+/** API **/
+
+var TWITTER_BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback) {
+  var settings = {
+    url: YOUTUBE_BASE_URL,
+    data: {
+      q: searchTerm,
+      part: 'snippet',
+      key: 'AIzaSyAuB6kqL52NjpIT8qi8y1MasDE5OXcjz7g'
+    }, 
+    dataType: 'json',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
+}
+
+function displayData() {
+	console.log("Display Data");
+}
+
+
+
+/** EVENT LISTENERS**/
+
 $('input[type="text"').on('keypress', function(event){
 	if(event.keyCode == 13) {
 		event.preventDefault();
-		console.log($(event.currentTarget).val());
+		var keyword = $(event.currentTarget).val();
+		$('.tweet-list').removeClass('hidden');
+		getDataFromApi(keyword, displayData);
 	}
-	
 });
 
 }
