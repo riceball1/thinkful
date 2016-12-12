0b5a25b0ab827bd47674cc96d0f41668d7d4a51f
Dana Ng <ngdana1@gmail.com>
2016-12-12 17:14:43 +0300

    updated app.js and index with new ajax call

--- capstone1-v2/app.js
+++ capstone1-v2/app.js
@@ -1,2 +1,21 @@
-"use strict";
+var BASE_URL = 'https://morning-ravine-82145.herokuapp.com/something';
 
+function getDataFromApi(callback) {
+  var settings = {
+    url: BASE_URL,
+    dataType: 'jsonp',
+    type: 'GET',
+    success: callback,
+    crossdomain: true
+  };
+  $.ajax(settings);
+}
+
+function displaySearchData(data) {
+  console.log(data);
+}
+
+
+$('.btn').click(function() {
+  getDataFromApi(displaySearchData);
+});
