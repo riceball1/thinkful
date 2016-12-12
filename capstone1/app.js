3b6d6dc845f76101df0e5107d35e915d80225765
Dana Ng <ngdana1@gmail.com>
2016-12-12 23:01:25 +0300

    added displayTweetData and added searchTerm to state

--- capstone1/app.js
+++ capstone1/app.js
@@ -2,11 +2,8 @@
 
 function tweeterList() {
   console.log("Document is ready!");
-  
-
   var TWITTER_BASE_URL = 'https://morning-ravine-82145.herokuapp.com/';
-  var YANDEX_TRANSLATE_URL = 'https://translation.googleapis.com/language/translate/v2?parameters';
-
+  var YANDEX_TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate?';
 
   function getDataFromTwitter(searchTerm, callback) {
     $.ajax({
@@ -17,45 +14,54 @@ function tweeterList() {
     });
   }
 
-  function displayTwitterData(data) {
-    // get data into state
-    for(var i = 0; i < 5; i++) {
-      console.log(data.statuses[i]);
+  function displayTweetData(data, lengthToDisplay=10) {
+    var resultElement = '';
+    for(var i = 0; i < lengthToDisplay; i++) {
+        // do stuff here to render onto html
+    }
+    $('.individual-tweets-list').html(resultElement);
   }
-    
+
   function getDataFromYandex(text, callback) {
     $.ajax({
       url: YANDEX_TRANSLATE_URL,
       dataType: 'jsonp',
+      key: 'trnsl.1.1.20161212T153919Z.2c641cfa919addd2.4255914407af1a02e8559664527d9d96260beeca',
       success: callback,
       type: 'GET'
     });
   }
 
 
-
-
-  }
-
-
-
-
+// create a place for the data to be held
+  var state = {
+    searchTerm: ''
+  };
 
   /** EVENT LISTENERS**/
 
   // watch for enter in keyword
   $('input[type="text"').on('keypress', function(event){
+    // watches for 'enter'
   	if(event.keyCode == 13) {
   		event.preventDefault();
-  		var keyword = { q: $(event.currentTarget).val() };
+      var word = $(event.currentTarget).val();
+  		var keyword = { q: word };
+      // keep searchTerm in state
+      state.searchTerm = word;
       console.log("Keyword: ", keyword);
+      console.log(word);
   		$('.tweet-list').removeClass('hidden');
-  		getDataFromTwitter(keyword, displayTwitterData);
+      // clears the results when doing a new search
+      resultElement = '';
+  		getDataFromTwitter(keyword, displayTweetData);
   	}
   });
 
-   // watch for translate
-   // getDataFromGoogleTranslate()
+  $('.translator').click(function() {
+    var text = $('.js-google-translate')[0].innerText;
+    console.log(text);
+  });
 
 }
 
