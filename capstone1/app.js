8e7523514abe1e2d4b5bfc11e8ce7759a80c000e
Dana Ng <ngdana1@gmail.com>
2016-12-13 18:13:19 +0300

    updated translation request, working but not all languages

--- capstone1/app.js
+++ capstone1/app.js
@@ -3,7 +3,7 @@
 function tweeterList() {
   console.log("Document is ready!");
   var TWITTER_BASE_URL = 'https://morning-ravine-82145.herokuapp.com/';
-  var YANDEX_TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json/detect?';
+  var YANDEX_TRANSLATE_URL = 'https://translate.yandex.net/api/v1.5/tr.json/translate';
 
   function getDataFromTwitter(searchTerm, callback) {
     $.ajax({
@@ -41,20 +41,25 @@ function tweeterList() {
 
   function getDataFromYandex(text, language, callback) {
     $.ajax({
-      // url: YANDEX_TRANSLATE_URL+ "&key=" + this.key + "&text=" + this.text + "&lang=" + this.lang,
-      // dataType: 'jsonp',
-      // key: 'trnsl.1.1.20161212T153919Z.2c641cfa919addd2.4255914407af1a02e8559664527d9d96260beeca',
-      // success: callback,
-      // text: text,
-      // type: 'POST',
-      // lang: language,
-      // error: function(e) {
-      //   console.log(e);
-      // }
-      url: 'https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20140720T191145Z.05605441c6ee16dc.eaaf6c6c8690cb5fb094cea2bfec4f787af6170c&lang=ru&text=C%27est+la+question',
-      dataType: "jsonp",
-      success: function (data) {
-        console.log(data);
+      url: YANDEX_TRANSLATE_URL,
+      dataType: 'json',
+      data:{
+          key: 'trnsl.1.1.20161212T153919Z.2c641cfa919addd2.4255914407af1a02e8559664527d9d96260beeca',
+          lang: language,
+          text: text,
+      },
+      success: callback,
+
+      type: 'GET',
+      ui: language,
+      format: "html",
+      success: function (response) {
+        console.log(this.url)
+        $('.translated-text').html(response.text[0])  //translated text. 
+      },
+      error: function(error) {
+        console.log(this.url)
+        console.log(error);
       }
     });
   }
@@ -124,12 +129,12 @@ function tweeterList() {
       state.language = lang;
       console.log(state.language);
       $('.language-choice').html(langName);
-
   });
 
   $('.translate-me').click(function() {
     var text = $('.original-text').text();
-    var language = 'ru';
+    var language = state.language;
+    console.log("This is in translate me: " + language);
     getDataFromYandex(text, language, displayYandexData);
   });
 
