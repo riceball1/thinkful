83bd4d64fbc9d8873565ea565c6f57cd24439ac2
Dana Ng <ngdana1@gmail.com>
2016-12-27 00:35:21 -0700

    wrapped functions and event listeners in a single function and added document ready call

--- logo-search/app.js
+++ logo-search/app.js
@@ -1,24 +1,25 @@
-function displaySearchItem(event){
-  var currentItem = $(event.currentTarget)
-  var selection = $(currentItem).val();
-  var parentBtn = $(currentItem).parent().parent();
-  var selectBtn = $(parentBtn)[0].childNodes[1];
-  $(selectBtn).text(selection);
+function searchLogos(){
+  function displaySearchItem(event){
+    var currentItem = $(event.currentTarget)
+    var selection = $(currentItem).val();
+    var parentBtn = $(currentItem).parent().parent();
+    var selectBtn = $(parentBtn)[0].childNodes[1];
+    $(selectBtn).text(selection);
+  }
+
+  /** EVENT LISTENERS **/
+  $('.dropdown-content').on('click', 'option', function(event){
+    event.preventDefault();
+    displaySearchItem(event);
+    // send selectBtn info to a function that will do a filter
+    // and color the logos that match the query
+  });
+
+  $('.search-button').on('click', function(event){
+    event.preventDefault();
+    var inputValues = $('.dropbtn').text();
+    console.log(inputValues);
+  });
 }
 
-
-/** EVENT LISTENERS **/
-
-$('.dropdown-content').on('click', 'option', function(event){
-  event.preventDefault();
-  displaySearchItem(event);
-  // send selectBtn info to a function that will do a filter
-  // and color the logos that match the query
-});
-
-$('.search-button').on('click', function(event){
-  event.preventDefault();
-  var inputValues = $('.dropbtn').text();
-  console.log(inputValues);
-
-});
+$(document).ready(searchLogos);
