42f98eb84a4e2342a2caac68f164d94f1c709df7
Dana Ng <ngdana1@gmail.com>
2017-01-10 13:02:33 -0800

    added blog-api challenge

---
+++ blog-api/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList, Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
