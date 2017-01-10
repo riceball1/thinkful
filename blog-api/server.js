20b1f8726cce9679657bdf2019b01c0a1c86d697
Dana Ng <ngdana1@gmail.com>
2017-01-10 14:23:36 -0800

    deleted severing of static website - not needed

--- blog-api/server.js
+++ blog-api/server.js
@@ -11,12 +11,6 @@ const app = express();
 // log the http layer
 app.use(morgan('common'));
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
-});
-
 /* when requests comes into /blog-api
  we'll route them to the express router instance
  These router instances act as modular, mini-express apps.
